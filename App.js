3c942d1d029a28aae29155cd4eb882bf22c36a4f
actjacob <yakupaktar99@gmail.com>
2025-05-02 23:55:23 +0300

    Stillendirme konusuna giriş yapıldı

--- App.js
+++ App.js
@@ -1,28 +1,30 @@
 import React from 'react';
-import {View, Text, SafeAreaView, Button, Alert} from 'react-native';
+import {View, Text, SafeAreaView, StyleSheet} from 'react-native';
 
 const App = () => {
   // function sayHello() {
   //   console.log('Merhaba');
   // }
 
-  const sayHello = label => {
-    console.log('Merhabalar!!' + label);
-  };
+  // const sayHello = label => {
+  //   console.log('Merhabalar!!' + label);
+  // };
   return (
     <SafeAreaView>
-      <View style={{backgroundColor: 'green'}}>
+      <View style={styles.container}>
         <Text>Hello World!</Text>
-        <Text>Hello World2!</Text>
       </View>
-      <Text>Hello World3!</Text>
-      <Button
-        title="Press Me"
-        onPress={() => sayHello('yakup')}
-        color={'crimson'}
-      />
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'red',
+    margin: 10,
+    padding: 10,
+    borderRadius: 5,
+  },
+});
 
 export default App;
